19d907af388e42b7f8a11b1f296fc789b3cd7a29
HMPandt <302831888@student.rocmondriaan.nl>
2022-09-01 12:49:24 +0200

    4.3

--- hoofdstuk4/opdr4.3.php
+++ hoofdstuk4/opdr4.3.php
@@ -6,7 +6,14 @@
 <body>
 <p>
     <?php
-
+    $first = 32;
+    $second = 21;
+    if($first > $second){
+        $uitKomst = $first * 2 + $second;
+    } else {
+        $uitKomst = $second * 2 + $first;
+    }
+    echo "uitkomst = $uitKomst";
     ?>
 </p>
 </body>
